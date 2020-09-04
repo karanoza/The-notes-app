307ebd8d048df30b8fc5e5d2e47e9fccfa7a889f
Karan <karanoza94@gmail.com>
2020-09-04 18:05:18 +0530

    remove note

--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-
+const chalk = require("chalk");
 const getNotes = function () {
   return "Your notes...";
 };
@@ -22,6 +22,20 @@ const addNote = function (title, body) {
   }
 };
 
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("no note found"));
+  }
+};
+
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
@@ -40,4 +54,5 @@ const loadNotes = function () {
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
+  removeNote: removeNote,
 };
